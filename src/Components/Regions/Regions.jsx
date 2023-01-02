38b2c282da0f1a1232fb7f123a7eafa5901903d8
kweeka <mtalbi29@yahoo.com>
2023-01-02 20:31:33 +0100

    Add Regions component

---
+++ src/Components/Regions/Regions.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+import './regions.scss';
+import brazilMap from '../../Assets/brazil.png';
+import russiaMap from '../../Assets/russia.png';
+import europeMap from '../../Assets/europe.png';
+import koreaMap from '../../Assets/korea.png';
+import japanMap from '../../Assets/japan.png';
+import northAmericaMap from '../../Assets/northAmerica.png';
+
+const Regions = ({ regions }) => {
+  const regionMaps = {
+    Brazil: brazilMap,
+    Russia: russiaMap,
+    Europe: europeMap,
+    Korea: koreaMap,
+    Japan: japanMap,
+    'North America': northAmericaMap,
+  };
+
+  return (
+    <div className="regions">
+      {
+        regions.length && regions.map((region) => (
+          Object.keys(region).map((key) => (
+            <div key={key} className="region">
+              <figure>
+                <img className="region-map" src={regionMaps[key]} alt={`${key} region map`} />
+              </figure>
+              <span className="region-name">{key}</span>
+            </div>
+          ))
+        ))
+      }
+    </div>
+  );
+};
+
+export default Regions;
