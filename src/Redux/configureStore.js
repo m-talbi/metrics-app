a29559107df81b5a290c99e6fa596b5f5e69b83a
kweeka <mtalbi29@yahoo.com>
2023-01-04 13:34:17 +0100

    Add player slice to store

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import playerSlice from './player/playerSlice';
 import regionsSlice from './regions/regionsSlice';
 
 const store = configureStore({
   reducer: {
     regions: regionsSlice,
+    player: playerSlice,
   },
 });
 
